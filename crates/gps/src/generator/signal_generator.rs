3b7e9e127046b482f0484c17bbe52fed72717bed
Lao Lilin <i@laolilin.com>
2025-04-18 23:53:24 +0800

    refactor(eph): move generate_navigation_subframes to Ephemeris impl
    - Migrate navigation message generation from utils module to Ephemeris method
    - Update call sites in signal_generator and channel allocation code
    - Maintain existing ICD-200L compliant bit-packing logic and parameter scaling

--- crates/gps/src/generator/signal_generator.rs
+++ crates/gps/src/generator/signal_generator.rs
@@ -14,7 +14,7 @@ use crate::{
     eph::Ephemeris,
     ionoutc::IonoUtc,
     table::{ANT_PAT_DB, COS_TABLE512, SIN_TABLE512},
-    utils::{allocate_channel, compute_range, generate_navigation_subframes},
+    utils::{allocate_channel, compute_range},
 };
 #[derive(Debug)]
 pub struct SignalGenerator {
@@ -381,12 +381,12 @@ impl SignalGenerator {
                             for ichan in channels.iter_mut().take(MAX_CHAN) {
                                 // Generate new subframes if allocated
                                 if ichan.prn != 0_i32 {
-                                    generate_navigation_subframes(
-                                        &ephemerides[*ieph]
-                                            [(ichan.prn - 1) as usize],
-                                        ionoutc,
-                                        &mut ichan.sbf,
-                                    );
+                                    ephemerides[*ieph]
+                                        [(ichan.prn - 1) as usize]
+                                        .generate_navigation_subframes(
+                                            ionoutc,
+                                            &mut ichan.sbf,
+                                        );
                                 }
                             }
                         }
